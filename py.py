1cdc2bcd9bf0aa63da75abafa9bfb0602cc4b516
Waltz Fork <43420385+theblackwaltz@users.noreply.github.com>
2020-07-28 18:38:13 +0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,13 @@
+import time
+x = int(input("enter a number = "))
+y = range(1,x)
+z = []
+for m in y:
+    time.sleep(0.3)
+    print("trying to add more ")
+    if x%m==0:
+        print (m)
+        z.append(m)
+    else:
+        pass
+    print(z)
---
+++ py1.py
@@ -0,0 +1,7 @@
+#Is a list Palindrome Or Not By Arda Duman.
+x=input("enter a string :")
+y=x[::-1]
+if x == y:
+    print("yes it is palindrome")
+else:
+    print("sorry it is not")
